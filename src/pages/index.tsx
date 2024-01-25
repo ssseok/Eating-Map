4074175cd1d177d927358e337168eddc20138814
Hyun Seok Park <luckseok1@gmail.com>
2024-01-25 21:30:10 +0900

    :sparkles: Kakao marker 선택한 식당 UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,25 @@
 import Map from '@/components/Map';
+import Markers from '@/components/Markers';
+import StoreBox from '@/components/StoreBox';
+import * as stores from '@/data/store_data.json';
+import { useState } from 'react';
 
 export default function Home() {
+  const [map, setMap] = useState(null);
+  const [currentStore, setCurrentStore] = useState(null);
+  const storeDatas = stores['DATA'];
   return (
     <>
-      <Map />
+      <Map setMap={setMap} />
+      <Markers
+        storeDatas={storeDatas}
+        map={map}
+        setCurrentStore={setCurrentStore}
+      />
+      <StoreBox
+        store={currentStore}
+        setStore={setCurrentStore}
+      />
     </>
   );
 }
