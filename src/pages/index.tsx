9869a395ea2d7c0988a9230d40e8c956a6124e94
Hyun Seok Park <luckseok1@gmail.com>
2024-01-26 17:51:34 +0900

    :sparkles: Api 생성하여 getStaticProps, getServerSideProps 으로 데이터 가져오기

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,19 @@
 import Map from '@/components/Map';
 import Markers from '@/components/Markers';
 import StoreBox from '@/components/StoreBox';
-import * as stores from '@/data/store_data.json';
+
+import { StoreType } from "@/interface";
 import { useState } from 'react';
 
-export default function Home() {
+export default function Home({ stores }: {stores : StoreType[]}) {
   const [map, setMap] = useState(null);
   const [currentStore, setCurrentStore] = useState(null);
-  const storeDatas = stores['DATA'];
+  
   return (
     <>
       <Map setMap={setMap} />
       <Markers
-        storeDatas={storeDatas}
+        stores={stores}
         map={map}
         setCurrentStore={setCurrentStore}
       />
@@ -22,4 +23,13 @@ export default function Home() {
       />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const stores = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`).then((res) => res.json());
+
+  return {
+    props : {stores},
+    revalidate: 60 * 60, // 1시간 마다 업데이트
+  }
 }
