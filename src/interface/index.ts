ec2df68975f007642aa6fe41e71224994715484a
Hyun Seok Park <luckseok1@gmail.com>
2024-02-08 20:00:56 +0900

    :sparkles: Prisma 데이터 seed 실습

--- src/interface/index.ts
+++ src/interface/index.ts
@@ -1,10 +1,10 @@
 export interface StoreType {
-  tel_no: string;
-  cob_code_nm: string;
-  bizcnd_code_nm: string;
-  upso_nm: string;
-  x_cnts: string;
-  y_dnts: string;
-  rdn_code_nm: string;
-  crtfc_gbn_nm: string;
+    tel_no: string;
+    cob_code_nm: string;
+    bizcnd_code_nm: string;
+    upso_nm: string;
+    x_cnts: string;
+    y_dnts: string;
+    rdn_code_nm: string;
+    crtfc_gbn_nm: string;
 }
